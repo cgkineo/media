d2f3bdcb63e650a2894e628390d46f831bb58af1
oliverfoster <oliver.foster@kineo.com>
2018-10-28 01:19:02 +0100

    Added sensible size overrides for auto values

--- src/js/mediaui/outputs/resize.js
+++ src/js/mediaui/outputs/resize.js
@@ -16,32 +16,76 @@ MediaUI.Output.Resize = MediaUI.Output.extend({
   },
 
   onResize$bind: function(event) {
-    this.resize();
-    this.resizeinner();
-  },
+    var uiDimensions = this.getUIDimensions();
+    var uiOffsetParent = this.ui.el.offsetParent || window;
+    var uiParentDimensions = new Media.Class.Dimensions(uiOffsetParent);
+    var uiConfig = uiDimensions[this.ui.fullscreen.isActive ? 'fullscreen' : 'normal'];
 
-  resize: function() {
+    // UI config exceptions
+    var isUIAutoAuto = uiConfig.height.unit === "auto" && uiConfig.width.unit === "auto";
+    if (isUIAutoAuto) {
+      uiConfig.height.value = "contain";
+      uiConfig.height.unit = "contain";
+      uiConfig.width.value = "contain";
+      uiConfig.width.unit = "contain";
+    }
+    var isUICover = uiConfig.height.value === "cover";
+    if (isUICover) {
+      uiConfig.height.value = "fill";
+      uiConfig.height.unit = "fill";
+      uiConfig.width.value = "fill";
+      uiConfig.width.unit = "fill";
+    }
+
     this.$els.forEach(function(el) {
-      var uiDimensions = this.getUIDimensions();
-      var uiOffsetParent = this.ui.el.offsetParent || window;
-      var uiParentDimensions = new Media.Class.Dimensions(uiOffsetParent);
-      var uiConfig = uiDimensions[this.ui.fullscreen.isActive ? 'fullscreen' : 'normal'];
       var uiStyle = this.getStyles(el, this.ui.source, uiParentDimensions, uiConfig);
       for (var k in uiStyle) {
         rafer.set(el.style, k, uiStyle[k]);
       }
     }.bind(this));
-  },
 
-  resizeinner: function() {
+    var isUISourceParent = Boolean(elements(this.ui.source).path().find(function(item) {
+      return item === this.ui.el;
+    }.bind(this)));
+    var sourceOffsetParent = isUISourceParent ? this.ui.el.offsetParent : this.ui.source.offsetParent;
+    var sourceParentDimensions =  new Media.Class.Dimensions(sourceOffsetParent || window);
+    var sourceConfig = event[this.ui.fullscreen.isActive ? 'fullscreen' : 'normal'];
+
+    // Source config exceptions
+    var usePxValues = false;
+    if (isUISourceParent) {
+      switch (uiConfig.height.value) {
+        case "contain":
+          var isSourceFillOrCover = sourceConfig.height.value === "fill" || sourceConfig.height.value === "cover";
+          var isSourceAutoAuto = sourceConfig.height.unit === "auto" && sourceConfig.width.unit === "auto";
+          if (isSourceFillOrCover || isSourceAutoAuto) {
+            sourceConfig.height.value = "contain";
+            sourceConfig.height.unit = "contain";
+            sourceConfig.width.value = "contain";
+            sourceConfig.width.unit = "contain";
+          }
+          break;
+        case "fill":
+          var isSourceAutoAuto = sourceConfig.height.unit === "auto" && sourceConfig.width.unit === "auto";
+          if (isSourceAutoAuto) {
+            sourceConfig.height.value = "cover";
+            sourceConfig.height.unit = "cover";
+            sourceConfig.width.value = "cover";
+            sourceConfig.width.unit = "cover";
+          }
+          break;
+        case "retain":
+          sourceConfig.height.value = "retain";
+          sourceConfig.height.unit = "retain";
+          sourceConfig.width.value = "retain";
+          sourceConfig.width.unit = "retain";
+          break;
+      }
+    }
+
     this.$elsinners.forEach(function(el) {
-      var isUISourceParent = Boolean(elements(this.ui.source).path().find(function(item) {
-        return item === this.ui.el;
-      }.bind(this)));
-      var sourceOffsetParent = isUISourceParent ? this.ui.el.offsetParent : this.ui.source.offsetParent;
-      var sourceParentDimensions =  new Media.Class.Dimensions(sourceOffsetParent || window);
-      var sourceConfig = event[this.ui.fullscreen.isActive ? 'fullscreen' : 'normal'];
       var sourceStyle = this.getStyles(el, this.ui.source, sourceParentDimensions, sourceConfig, {
+        usePxValues: usePxValues,
         transform: Media.device.isIE11 ? "scale(1.02)" : ""
       });
       for (var k in sourceStyle) {
@@ -56,26 +100,27 @@ MediaUI.Output.Resize = MediaUI.Output.extend({
 
     var style = {};
     style['object-fit'] = "fill";
+
     switch (dimensions.width.unit) {
       case "contain":
         if (dimensions.ratio <= parentDimensions.ratio) {
           // Full height
-          style.height = "100%";
+          style.height = options.usePxValues ? makeUnit(parentDimensions.height.value, parentDimensions.height.unit) : "100%";;
           style.width = makeUnit((parentDimensions.height.value * dimensions.ratio), parentDimensions.height.unit);
         } else {
           // Full width
-          style.width = "100%";
+          style.width = options.usePxValues ? makeUnit(parentDimensions.width.value, parentDimensions.width.unit) : "100%";
           style.height = makeUnit((parentDimensions.width.value / dimensions.ratio), parentDimensions.width.unit);
         }
         break;
       case "cover":
         if (dimensions.ratio <= parentDimensions.ratio) {
           // Full width
-          style.width = "100%";
+          style.width = options.usePxValues ? makeUnit(parentDimensions.width.value, parentDimensions.width.unit) : "100%";
           style.height = makeUnit((parentDimensions.width.value / dimensions.ratio), parentDimensions.width.unit);
         } else {
           // Full height
-          style.height = "100%";
+          style.height = options.usePxValues ? makeUnit(parentDimensions.height.value, parentDimensions.height.unit) : "100%";
           style.width = makeUnit((parentDimensions.height.value * dimensions.ratio), parentDimensions.height.unit);
         }
         break;
@@ -94,8 +139,8 @@ MediaUI.Output.Resize = MediaUI.Output.extend({
         break;
       case "fill":
         // Full height and width
-        style.height = "100%";
-        style.width = "100%";
+        style.width = options.usePxValues ? makeUnit(parentDimensions.width.value, parentDimensions.width.unit) : "100%";;
+        style.height = options.usePxValues ? makeUnit(parentDimensions.height.value, parentDimensions.height.unit) : "100%";;
         break;
       case "none":
         style.height = "";
